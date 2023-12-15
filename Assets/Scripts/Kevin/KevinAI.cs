74533817f5597216a3556fe3ce6ffa4d09ba82e7
Thomas Shaw <printer.83mph@gmail.com>
2023-12-15 01:38:30 -0500

    Add full hospital level

--- Assets/Scripts/Kevin/KevinAI.cs
+++ Assets/Scripts/Kevin/KevinAI.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 namespace Kevin
 {
@@ -7,61 +12,102 @@ namespace Kevin
     {
         private abstract class KevinState
         {
-            protected KevinAI AI;
+            protected readonly KevinAI AI;
 
             protected KevinState(KevinAI ai)
-            {
-                AI = ai;
-            }
-            public abstract void Update();
+            { AI = ai; }
+
+            public virtual void OnStateEnd()
+            { }
+            
+            public virtual void Update()
+            { }
+
+            public virtual void OnStateStart()
+            { }
         }
 
-        private class StalkState : KevinState
+        private class WanderState : KevinState
         {
-            private readonly KevinTarget[] _targets = FindObjectsOfType<KevinTarget>();
-            private int _targetIndex = 0;
+            private readonly KevinTarget[] _targets = new KevinTarget[3];
 
-            private float _waitTime;
+            public WanderState(KevinAI ai) : base(ai)
+            { }
 
-            public StalkState(KevinAI ai) : base(ai)
-            {}
-
-            public override void Update()
+            public override void OnStateStart()
             {
-                _waitTime -= Time.deltaTime;
-                if (_waitTime < 0f)
-                {
-                    _waitTime = 10f;
-                    _targetIndex = (_targetIndex + 1) % _targets.Length;
-                    AI._agent.SetDestination(_targets[_targetIndex].transform.position);
-                }
-
-                AI._agent.speed = AI._agent.remainingDistance < 6f ? AI.walkSpeed : AI.runSpeed;
+                AI._agent.speed = AI.walkSpeed;
+                AI.StartCoroutine(TargetsLoop(AI));
+                AI.StartCoroutine(WanderLoop(AI));
             }
+
+            private IEnumerator TargetsLoop(KevinAI ai)
+            {
+                while (true)
+                {
+                    Debug.Log("Hello");
+                    Debug.Log(ai);
+                    // set three targets (farthest from player)
+                    Array.Sort(ai._targets, (a, b) => (int)Mathf.Sign(
+                        (b.transform.position - ai._camera.transform.position).sqrMagnitude -
+                        (a.transform.position - ai._camera.transform.position).sqrMagnitude)
+                    );
+
+                    for (var i = 0; i < 3; i++)
+                        _targets[i] = ai._targets[i];
+
+                    yield return new WaitForSeconds(1);
+                }
+            }
+
+            private IEnumerator WanderLoop(KevinAI ai)
+            {
+                while (true)
+                {
+                    var target = Mathf.FloorToInt(Random.value * 3f);
+                    ai._agent.SetDestination(_targets[target].transform.position);
+                    yield return new WaitForSeconds(Random.Range(4.5f, 20f));
+                }
+            }
+
         }
         
+        // parameters
         [SerializeField] private float walkSpeed = 2f;
         [SerializeField] private float runSpeed = 4f;
 
         public float WalkSpeed => walkSpeed;
         public float RunSpeed => runSpeed;
 
+        // references
         private NavMeshAgent _agent;
+        private Camera _camera;
+        private KevinTarget[] _targets;
 
+        // internal logic
         private KevinState _state;
+
+        private void SetState(KevinState newState)
+        {
+            _state?.OnStateEnd();
+            StopAllCoroutines();
+            _state = newState;
+            newState.OnStateStart();
+        }
 
         private void Awake()
         {
             _agent = GetComponent<NavMeshAgent>();
+            _camera = Camera.current;
+            _targets = FindObjectsOfType<KevinTarget>();
         }
 
         private void Start()
         {
-            _state = new StalkState(this);
+            SetState(new WanderState(this));
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             _state.Update();
         }
